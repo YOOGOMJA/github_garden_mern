78e112be2ea5871cccf760ed23b6e6cec97c379d
KyeongSoo Yoo <dev.yoogomja@gmail.com>
2020-08-28 15:09:25 +0900

    회원 가입 후 크롤링 추가

--- db/passport/strategies/github.js
+++ db/passport/strategies/github.js
@@ -1,39 +1,48 @@
-import passportGithub from 'passport-github2';
-import config from '../../config.json';
-import { User } from '../../models';
+import passportGithub from "passport-github2";
+import config from "../../config.json";
+import { User } from "../../models";
+import * as Crawler from "../../compute/crawling";
 
-export default new passportGithub.Strategy({
-    clientID : config.github.oauth.client_id,
-    clientSecret : config.github.oauth.client_secret,
-    callbackURL : `${ config.host.server[process.env.NODE_ENV] }/auth/callback`
-},
-    async function( accessToken, refreshToken, profile, cb ){
-        try{    
+export default new passportGithub.Strategy(
+    {
+        clientID: config.github.oauth.client_id,
+        clientSecret: config.github.oauth.client_secret,
+        callbackURL: `${
+            config.host.server[process.env.NODE_ENV]
+        }/auth/callback`,
+    },
+    async function (accessToken, refreshToken, profile, cb) {
+        try {
             const user = await User.findOne({
-                id : profile.id
+                id: profile.id,
             });
-            if(user){ 
-                await User.updateOne({
-                    id : profile.id,
-                },
-                {
-                    ...profile._json,
-                    access_token : accessToken,
-                    refresh_token : refreshToken,
-                });
-                return cb(null, user);  }
-            else{
+            if (user) {
+                await User.updateOne(
+                    {
+                        id: profile.id,
+                    },
+                    {
+                        ...profile._json,
+                        access_token: accessToken,
+                        refresh_token: refreshToken,
+                    }
+                );
+
+                return cb(null, user);
+            } else {
                 const newUser = new User({
                     ...profile._json,
-                    access_token : accessToken,
-                    refresh_token : refreshToken,
+                    access_token: accessToken,
+                    refresh_token: refreshToken,
                 });
                 await newUser.save();
+
+                await Crawler.one(profile.login);
+
                 return cb(null, newUser);
             }
-        }
-        catch(err){
+        } catch (err) {
             return cb(err);
         }
     }
-)
+);
